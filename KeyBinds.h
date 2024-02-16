a4d6f2eb37810cde3a474195a2110d61983b712e
eldon-chung <eldon.chung.yr@gmail.com>
2024-02-16 23:59:59 +0800

    add KeyBinds with debugging statements

--- KeyBinds.h
+++ KeyBinds.h
@@ -7,24 +7,37 @@
 #include <utility>
 #include <vector>
 
+#include <iostream>
+
 #include "text_buffer.h"
 #include "view.h"
 
 // list all the handler functions here?
 
-struct KeyInfo {
-    // first 32 bits are id, remaining bits are modifiers
-    uint64_t id_modifiers;
-
-    KeyInfo(ncinput input)
-        : id_modifiers(((uint64_t)input.id << 32) | input.modifiers) {}
-
-    KeyInfo(KeyInfo const &) = default;
-    KeyInfo(KeyInfo &&) = default;
-    KeyInfo &operator=(KeyInfo const &) = default;
-    KeyInfo &operator=(KeyInfo const &) = default;
-};
+void null_func() {}
 
 struct KeyBinds {
-    std::vector<std::pair<KeyInfo, std::function<void()>>> handler_list;
+    std::array<std::function<void()>, 1024> handler_list;
+
+    KeyBinds() = default;
+
+    std::function<void()> operator[](ncinput input) {
+        size_t index = input.id;
+        if (index >= preterunicode(1) && index <= preterunicode(12)) {
+            // maps 1, 12 to 0, 11
+            index -= preterunicode(1);
+        } else if (index >= preterunicode(21) && index <= preterunicode(30)) {
+            // maps 21, 30 to 12, 21
+            index -= preterunicode(21);
+            index += 12;
+        } else if (index == preterunicode(121)) {
+            // maps 121 to 13
+            index -= preterunicode(121);
+            index += 13;
+        }
+
+        std::cerr << "fn index: " << index << std::endl;
+
+        return std::function<void(void)>(null_func);
+    }
 };
--- main.cpp
+++ main.cpp
@@ -26,9 +26,16 @@ struct ProgramState {
         // move than text_buffer
         // so it has to apply a function
 
+        if (nc_input.id == NCKEY_RESIZE) {
+            exit(0);
+        }
+
+        std::cerr << "modifier: " << nc_input.modifiers << std::endl;
+        std::cerr << "id: " << nc_input.id << std::endl;
+
         // we're going to manually handled some cases to save on lookup
         if (nc_input.modifiers == 0 &&
-            ((nc_input.id >= nc_input.id >= 32 && nc_input.id <= 255) ||
+            ((nc_input.id >= 32 && nc_input.id <= 255) ||
              nc_input.id == NCKEY_TAB)) {
             text_buffer.insert_char((char)nc_input.id);
             return;
@@ -37,26 +44,20 @@ struct ProgramState {
         // TODO: handle all the other modifiers for these cases
         if (nc_input.modifiers == 0 && nc_input.id == NCKEY_BACKSPACE) {
             text_buffer.insert_backspace();
+            return;
         }
 
         if (nc_input.modifiers == 0 && nc_input.id == NCKEY_ENTER) {
             text_buffer.insert_newline();
+            return;
         }
 
         if (nc_input.modifiers == 0 && nc_input.id == NCKEY_DEL) {
             text_buffer.insert_delete();
-        }
-
-        // lookup for all the other cases? // should arrow keys be rebound?
-
-        // handle arrow keys
-        if (nc_input.id == NCKEY_LEFT || nc_input.id == NCKEY_RIGHT ||
-            nc_input.id == NCKEY_DOWN || nc_input.id == NCKEY_UP) {
-            text_buffer.move_cursor(nc_input.id - preterunicode(2));
             return;
         }
 
-        // keybinds_table[nc_input]();
+        keybinds_table[nc_input]();
     }
 
     void run_event_loop() {
